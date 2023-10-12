63853317c22196783b743de4fbf03f41839c12cd
Andrey <andrei4-09@mail.ru>
2023-10-12 13:26:35 +0300

    fix: pom was fixed

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,14 +2,13 @@ drop table IF EXISTS users CASCADE;
 drop table IF EXISTS authorities CASCADE;
 
 CREATE TABLE IF NOT EXISTS users(
-                      username varchar_ignorecase(50) not null primary key,
-                      password varchar_ignorecase(50) not null,
+                      username varchar(50) not null primary key,
+                      password varchar(50) not null,
                       enabled boolean not null
 );
 
 CREATE TABLE IF NOT EXISTS authorities (
-                             username varchar_ignorecase(50) not null,
-                             authority varchar_ignorecase(50) not null,
+                             username varchar(50) not null,
+                             authority varchar(50) not null,
                              constraint fk_authorities_users foreign key(username) references users(username)
 );
-create unique index IF NOT EXISTS ix_auth_username on authorities (username, authority);
