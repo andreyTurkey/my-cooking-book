12e53128a9e60e4891142d84d79103f0c39cf660
Andrey <andrei4-09@mail.ru>
2024-06-05 10:56:38 +0300

    перед удалением ветки mail

--- src/main/java/org/vaadin/example/VaadinSecurity/SecurityService.java
+++ src/main/java/org/vaadin/example/VaadinSecurity/SecurityService.java
@@ -4,9 +4,7 @@ import com.vaadin.flow.component.UI;
 import com.vaadin.flow.server.VaadinServletRequest;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Component;
 
